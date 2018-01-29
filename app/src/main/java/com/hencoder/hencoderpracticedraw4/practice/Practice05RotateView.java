c0d8ea64741da846497eeffc82764ba2befc8e93
黄建洪 <2372635607@qq.com>
2018-01-29 11:36:00 +0800

    [几何变换] 缩放scale,初始值1. 错切skew,初始值0.

--- app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice05RotateView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice05RotateView.java
@@ -17,6 +17,8 @@ public class Practice05RotateView extends View {
     Bitmap bitmap;
     Point point1 = new Point(200, 200);
     Point point2 = new Point(600, 200);
+    private int dh;
+    private int dw;
 
     public Practice05RotateView(Context context) {
         super(context);
@@ -32,13 +34,22 @@ public class Practice05RotateView extends View {
 
     {
         bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.maps);
+        dh = bitmap.getHeight()/2;
+        dw = bitmap.getWidth()/2;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+        canvas.save();
+        canvas.rotate(180, point1.x+ dw, point1.y + dh);
         canvas.drawBitmap(bitmap, point1.x, point1.y, paint);
+        canvas.restore();
+
+        canvas.save();
+        canvas.rotate(45, point2.x + dw, point2.y + dh);
         canvas.drawBitmap(bitmap, point2.x, point2.y, paint);
+        canvas.restore();
     }
 }
