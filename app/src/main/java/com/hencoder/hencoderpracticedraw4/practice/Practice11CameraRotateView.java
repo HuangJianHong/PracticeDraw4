84147370e07a1d12d28220470ce6176e8510a214
黄建洪 <2372635607@qq.com>
2018-01-29 17:11:09 +0800

    [camera] 绘制之三维坐标,虚拟对象camera使用;

--- app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice11CameraRotateView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice11CameraRotateView.java
@@ -3,6 +3,7 @@ package com.hencoder.hencoderpracticedraw4.practice;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Camera;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Point;
@@ -18,6 +19,8 @@ public class Practice11CameraRotateView extends View {
     Point point1 = new Point(200, 200);
     Point point2 = new Point(600, 200);
 
+    Camera camera = new Camera();
+
     public Practice11CameraRotateView(Context context) {
         super(context);
     }
@@ -32,13 +35,28 @@ public class Practice11CameraRotateView extends View {
 
     {
         bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.maps);
+
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        canvas.save();
+        camera.save();
 
+        camera.rotateX(20);
+        camera.applyToCanvas(canvas);
         canvas.drawBitmap(bitmap, point1.x, point1.y, paint);
+        camera.restore();
+        canvas.restore();
+
+        canvas.save();
+        camera.save();
+        camera.rotateY(26);
+        camera.applyToCanvas(canvas);
         canvas.drawBitmap(bitmap, point2.x, point2.y, paint);
+
+        camera.restore();
+        canvas.restore();
     }
 }
--- app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice12CameraRotateFixedView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice12CameraRotateFixedView.java
@@ -3,6 +3,7 @@ package com.hencoder.hencoderpracticedraw4.practice;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Camera;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Point;
@@ -17,6 +18,9 @@ public class Practice12CameraRotateFixedView extends View {
     Bitmap bitmap;
     Point point1 = new Point(200, 200);
     Point point2 = new Point(600, 200);
+    Camera camera = new Camera();
+    private int height;
+    private int width;
 
     public Practice12CameraRotateFixedView(Context context) {
         super(context);
@@ -32,13 +36,36 @@ public class Practice12CameraRotateFixedView extends View {
 
     {
         bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.maps);
+        height = bitmap.getHeight() / 2;
+        width = bitmap.getWidth() / 2;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        canvas.save();
+        camera.save();
+
+        camera.rotateX(30);        //x轴方向 旋转30度
+        canvas.translate(point1.x + width, point1.y + height);    //投影之后，返回到原先的位置，Canvas几何变换叠加要倒序;
+        camera.applyToCanvas(canvas);   //把旋转投影到Canvas
+        canvas.translate(-point1.x - width, -point1.y - height);   //投影之前将画布移动到三维坐标中心轴;
+        camera.restore();
 
         canvas.drawBitmap(bitmap, point1.x, point1.y, paint);
+        canvas.restore();
+
+
+        canvas.save();
+        camera.save();
+        camera.rotateY(50);
+        canvas.translate(point2.x + width, point2.y + height);
+        camera.applyToCanvas(canvas);
+        canvas.translate(-point2.x - width, -point2.y - height);
+
+        camera.restore();
         canvas.drawBitmap(bitmap, point2.x, point2.y, paint);
+        canvas.restore();
+
     }
 }
